e3c4924a3958e69975e34756ac5f4f4ed31ba454
Asher Seiling <f14asher@gmail.com>
2021-03-03 22:21:03 -0600

    Changed the cringe IF statements
    # Here, the elif (THIS ISNT WHAT ELIFS DO ITS A BAISC PRICIPAL OF PROGRAMMING AFTER THE FIRST IF ALL THE OTHERS ARE 'Else if' 'elif' is just a conbination of those IT CANNOT SERVE THE FUNCTION OF AN ELSE STATEMENT it only runs if it wasnt the previous if statement and it is equal to in this case 9 ) combines if and else. So if there is a number that's not 1 - 9, it will output this. Putting
    # multiple could lead to errors.

--- ankit.py
+++ ankit.py
@@ -10,26 +10,11 @@ answer = ''
 # This gets the random number
 ANSID = rn(1, 9)
 
-if ANSID == 1:
-    answer = 'Yes, definitely'
-if ANSID == 2:
-    answer = 'It is decidedly so'
-if ANSID == 3:
-    answer = 'Without a doubt'
-if ANSID == 4:
-    answer = 'Reply hazy, try again'
-if ANSID == 5:
-    answer = 'Ask again later'
-if ANSID == 6:
-    answer = 'Better if I do not tell you now.'
-if ANSID == 7:
-    answer = 'Outlook not so good'
-if ANSID == 8:
-    answer = 'My sources say no'
-# Here, the elif combines if and else. So if there is a number that's not 1 - 9, it will output this. Putting
+answers = ['Yes, definitely', 'It is decidedly so', 'Without a doubt', 'Reply hazy, try again', 'Ask again later', 'Better if I do not tell you now.', 'Outlook not so good', 'My sources say no', 'very doubtful']
+answer = answers[ANSID]
+# Here, the elif (THIS ISNT WHAT ELIFS DO ITS A BAISC PRICIPAL OF PROGRAMMING AFTER THE FIRST IF ALL THE OTHERS ARE 'Else if' 'elif' is just a conbination of those IT CANNOT SERVE THE FUNCTION OF AN ELSE STATEMENT it only runs if it wasnt the previous if statement and it is equal to in this case 9 ) combines if and else. So if there is a number that's not 1 - 9, it will output this. Putting
 # multiple could lead to errors.
-elif ANSID == 9:
-    answer = 'very doubtful'
+
 
 
 # This function gives you an answer
